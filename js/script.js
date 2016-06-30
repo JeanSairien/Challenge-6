06f6db869b30db8f4ac40ebbe8e020ed6315a67b
Yose <youness;chetoui@gmail.com>
2016-06-30 21:56:49 +0200

    Ajout image et background color, debut function multiplicateur +2

--- js/script.js
+++ js/script.js
@@ -6,4 +6,12 @@ function compteur(){
 	point +=1;
 	score = point;
 	document.getElementById("point").innerHTML=score;
+	//if (onclick multiplicateur)
+		//point +=2;
 };
+
+function multiplicateur() {
+	point +=2;
+	score = point;
+	document.getElementById("point").innerHTML=score;
+}
