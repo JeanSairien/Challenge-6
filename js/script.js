c19f711d8f491976c7bf159b2e548ddded1f75ec
Yose <youness.chetoui@gmail.com>
2016-07-03 13:03:00 +0200

    Le cliquere efectuer et fonctionel

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
-/*JS pour cookie click*/
-	var point =0;
-	var score;
+score = 0;
+Multiplicateur = 1;
 
-function compteur(){
-	point +=1;
-	score = point;
-	document.getElementById("point").innerHTML=score;
-	//if (onclick multiplicateur)
-		//point +=2;
-};
-
-function multiplicateur() {
-	point +=2;
-	score = point;
-	document.getElementById("point").innerHTML=score;
+function afficherScore() {
+  document.getElementById("score").innerHTML = "Score : " + score;
 }
+
+function afficherMulti() {
+  document.getElementById("multiplicateur").innerHTML = "Multiplicateur x" + Multiplicateur + " (prix du prochain : " + prix() + ")";
+  
+}
+
+function clic() {
+  score = score + Multiplicateur;
+  afficherScore();
+}
+
+function prix() {
+  return 15 * Multiplicateur * Multiplicateur;
+}
+
+function acheterMulti() {
+  if (score >= prix()) {
+    score = score - prix();
+    Multiplicateur = Multiplicateur + 1;
+    afficherMulti();
+    afficherScore();
+  } else {
+    alert("Cliquez plus !!");
+  }
+}
+
